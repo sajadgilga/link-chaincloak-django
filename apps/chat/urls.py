d33b2443a454bb3ae3bf7b240d94daa3b4875aac
sajadgilga <majidstic@yahoo.com>
2023-09-16 20:27:32 +0330

    Change url path

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.chat.apis.chat import ChatApi
 
 urlpatterns = [
-    path("chat/<str:service>", ChatApi.as_view(), name="chat"),
+    path("ask/<str:service>", ChatApi.as_view(), name="chat"),
 ]
